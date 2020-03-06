22f665fda8dbfaab68ec73f2f8390c2b451de64c
zeek3831 <36770333+zeek3831@users.noreply.github.com>
2020-03-06 18:16:02 -0500

    Add files via upload

---
+++ Stock_Dev.py
@@ -0,0 +1,49 @@
+#Created By BlessingGull250 AKA Ezekiel Bailey
+#IF you need help just email me at zeek.bailey3831@gmail.com with the subject "StockDev"
+from bs4 import BeautifulSoup
+import pandas as pd
+from alpha_vantage.timeseries import TimeSeries
+import numpy
+import requests
+import math
+import time
+import datetime
+import re
+
+
+t = str(datetime.datetime.today())
+ft = ("2015-01-01")
+print (t)
+print (ft)
+
+api_key = ('PS5YFD8WIR6IRHJ1')
+ts = TimeSeries(api_key, output_format='pandas')
+data = ts.get_intraday(symbol='ICLN', interval='1min', outputsize='compact')
+print (data)
+
+#Displaying
+p = (10)
+divp = (5.6)
+d = (divp / 4)
+pd = (p / 5)
+profit = (0)
+
+def display():
+    print ("\n########################")
+    print ("Dividend percentages")
+    print ("Quaterly")
+    print (d)
+    print ("Yearly")
+    print (divp)
+    print ("########################")
+    print ("Buy Price")
+    print (pd)
+    print ("########################")
+
+
+
+#Storing the Data
+#s.write(stock_1)
+#s.close
+#p.write(profit_1)
+#p.close
