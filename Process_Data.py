7fd8c842ae8c75e854b9e70ce459471a899a89d0
BlessingGull250 <36770333+zeek3831@users.noreply.github.com>
2020-06-16 15:57:54 -0400

    V1.1.3

--- Process_Data.py
+++ Process_Data.py
@@ -1,3 +1,15 @@
-import pandas
+import pandas as pd
 import csv
+import subprocess
 
+def clear():
+    subprocess.call("cls", shell=True)
+
+clear()
+
+
+
+data = pd.read_csv("DSE.csv")
+
+
+print(data['Date'])
